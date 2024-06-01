40a0941c722ba6f2f58ad0876e582b11aba69004
Martin Elsaesser <marels@aon.at>
2024-06-01 17:06:10 +0200

    Better explains hydration

--- src/helpers/renderUtil.tsx
+++ src/helpers/renderUtil.tsx
@@ -8,9 +8,25 @@ type RenderOptions = {
 // hydrate's arguments:
 // 1: a functional component
 // 2: props the functional component expects
-// 3: options for rendering, such paths to additional scripts, or the page title
+// 3: options for rendering, such as paths to additional scripts, or the page title
 //
-// hydrate passes the component to a layout, sets options within the layout and passes the props onto the client for hydration
+// what does hydrate do?
+// it wraps the component with a layout
+// sets the passed in options within the layout
+// and hydrates the component
+//
+// what does it mean to hydrate?
+// it means to pass the same data to a component on the server and on the client
+// both times it is used to create the UI
+// on the server the data is passed to the functional component to produce a string of HTML
+// on the client the data is used to recreate the functional component and update the UI
+//
+// how does this app hydrate?
+// the layout component takes the props data and saves it into a script tag on the page
+// it therefore creates a variable hydration Data which contains the data
+// <script>const hydrationData = {todos: ["todo1", "todo2"]}</script>
+//
+// a client component can then use this variable to recreate the state it was in when rendered on the server
 export function hydrate<T extends (...args: any[]) => any>(Component: T, props: Flatten<GetProps<T>>, options: RenderOptions) {
 	console.log(Component.name);
 	return (
